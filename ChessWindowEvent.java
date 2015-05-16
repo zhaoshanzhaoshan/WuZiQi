016b9d8d884e7b8bd7cfa56341786a8c3d17b0fa
zhaoshanzhaoshan <18396832569@163.com>
2015-05-16 22:42:20 +0800

    Project

---
+++ ChessWindowEvent.java
@@ -0,0 +1,17 @@
+package wuziqi;
+
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+	class ChessWindowEvent extends WindowAdapter{
+			public void windowClosing(WindowEvent e){
+		 System.exit(0);
+				}
+
+ChessWindowEvent()
+
+{
+}
+}
+
+
+
